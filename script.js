17966e8f77e228db74ca4945897095d500b2fe1d
milojezek <milekdeveloper@gmail.com>
2023-07-23 20:19:58 +0200

    Fix the margin of Projects section and remove some redundant code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Navigation: Hamburger selections
 const burger = document.querySelector("#hamburger-menu");
 const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
 
 burger.addEventListener("click", () => {
     ul.classList.toggle("show");
